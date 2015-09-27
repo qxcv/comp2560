902792941fd70433e1e845755b13fcd1ca1b054a
Sam Toyer <sam@qxcv.net>
2015-09-28 09:47:22 +1000

    Quick check script

---
+++ scripts/plot_all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This just plots all of the stuff in the "stats" directory, but with meaningful
+# titles
+
+./plot_pck.py --input "300 poses" ../stats/default-conf-300-poses.csv \
+    --input "50 poses" ../stats/default-conf-50-poses.csv \
+    --input "1 pose" ../stats/default-conf-1-pose.csv \
+    --input "Improved shoulders" ../stats/100-poses-no-colour-or-skin-on-shoulders.csv
