666497b5c3f538dc38ef846e6a0b1c2b38c837dd
Sam Toyer <sam@qxcv.net>
2015-09-29 10:43:43 +1000

    Remove junk data points

--- scripts/plot_all.sh
+++ scripts/plot_all.sh
@@ -6,9 +6,6 @@
 ./plot_pck.py --input "300 poses" "../stats/default-conf-300-poses.csv" \
     --input "50 poses" "../stats/default-conf-50-poses.csv" \
     --input "1 pose" "../stats/default-conf-1-pose.csv" \
-    --input "Improved shoulders" "../stats/100-poses-no-colour-or-skin-on-shoulders.csv" \
     --input "No skin/colour weights" "../stats/no-skin-or-colour-100-poses.csv" \
-    --input "No skin/colour, tweaked" "../stats/no-skin-or-colour-1-shoulder-sumflow-100-poses.csv" \
-    --input "No skin/colour, tweaked 2" "../stats/no-skin-or-colour-no-shoulder-motion-100-poses.csv" \
     --input "No skin/colour/motion" "../stats/no-skin-no-colour-no-motion-100-poses.csv" \
     $@
