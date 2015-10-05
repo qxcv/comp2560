a7918288c1018e4f8aaeb8f4c5b40e90f65a409d
Sam Toyer <sam@qxcv.net>
2015-10-06 00:46:48 +1100

    Make detection watching more useful

--- project/matlab/visualization/show_pose_sequence.m
+++ project/matlab/visualization/show_pose_sequence.m
@@ -1,18 +1,32 @@
 % display the detected pose sequence
-function show_pose_sequence(data_path, frames, detected_pose_seq, parent)
-figno=1023; figure(figno);clf(figno); 
+function show_pose_sequence(data_path, frames, detected_pose_seq, parent, dest_dir)
 
-for n = 1:length(frames)   
-    %video_showskeleton2(img{n}, detected_pose_seq(n,:));axis off;
+if nargin < 5
+    should_save = false;
+else
+    should_save = true;
+    mkdir(dest_dir);
+end
+
+figno=1023; figure(figno); clf(figno); 
+
+for n = 1:length(frames)
     video_showskeleton2([data_path frames(n).name], detected_pose_seq(n,:), parent);
     axis off;
-    pause(1);
+    if should_save
+        dest_path = fullfile(dest_dir, sprintf('frame-%i', n));
+        fprintf('Saving to %s\n', dest_path);
+        print(dest_path, '-dpng');
+        pause(0.2);
+    else
+        pause(1);
+    end
 end
 end
 
 function video_showskeleton2(frame, boxes, parent)
  img = imread(frame);
- imagesc(img); hold on; 
+ imshow(img); hold on; 
   
   for i = 1:length(parent)
     x1(:,i) = boxes(:,1+(i-1)*4);
@@ -24,13 +38,9 @@ function video_showskeleton2(frame, boxes, parent)
   y = (y1 + y2)/2;  
   
   hold on;
-  % line([x(1,2), x(1,4)], [y(1,2), y(1,4)],'color', 'r','linewidth', 3);
   line([x(1,3), x(1,5)], [y(1,3), y(1,5)],'color', 'r','linewidth', 3);
-  % line([x(1,4), x(1,6)], [y(1,4), y(1,6)],'color', 'b','linewidth', 3);
   line([x(1,5), x(1,7)], [y(1,5), y(1,7)],'color', 'b','linewidth', 3);
-  % line([x(1,7), x(1,9)], [y(1,7), y(1,9)],'color', 'g','linewidth', 3);
   line([x(1,11), x(1,13)], [y(1,11), y(1,13)],'color', 'g','linewidth', 3);
-  % line([x(1,9), x(1,11)], [y(1,9), y(1,11)],'color', 'y','linewidth', 3);
   line([x(1,13), x(1,15)], [y(1,13), y(1,15)],'color', 'y','linewidth', 3);
 
 hold off;
