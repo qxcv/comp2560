f39f6d29705924aebcf83f976df77860be466806
Sam Toyer <sam@qxcv.net>
2015-11-08 10:53:31 +1100

    Update results

--- scripts/plot_final.sh
+++ scripts/plot_final.sh
@@ -2,7 +2,7 @@
 
 dir="../stats/final"
 
-./plot_pck.py --input "Our method" "$dir/final-config.csv" \
+./plot_pck.py --input "Hybrid" "$dir/final-config.csv" \
     --input $'Chen and\nYuille' "$dir/final-config-static-only.csv" \
     --input "Cherian et al." "$dir/anoop-results.csv" \
     --input $'Yang and\nRamanan' "$dir/yang-ramanan-results.csv" \
