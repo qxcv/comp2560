6eda5ea19539a6a9ffb8b5b6bbd234ef9db9194f
Sam Toyer <sam@qxcv.net>
2016-05-24 09:57:18 +1000

    Fix get_framenum

--- project/matlab/utils/get_framenum.m
+++ project/matlab/utils/get_framenum.m
@@ -1,6 +1,11 @@
 % some function to return a unique id for a frame. unique in the dataset.
 function frameid = get_framenum(imname)
-   str = strtok(strrep(imname, '-', ''), '.');
-   str = str(str<'A'); 
-   frameid = str2double(str);
+% this just extracts the number at the end of the filename (e.g.
+% 'file-042-00013.png' --> 13)
+frame_match = regexp(imname, '[^\d](?<frame_no>\d+)\.png$', 'names');
+assert(numel(frame_match) == 1);
+str = frame_match.frame_no;
+%str = strtok(strrep(imname, '-', ''), '.');
+%str = str(str<'A');
+frameid = str2double(str);
 end
